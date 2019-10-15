d3f2e5fa615a6eeb5637d835a8605fc234fef121
ledasha <d.leanovich@intetics.com>
2019-10-15 10:22:12 +0300

    Added first step

---
+++ src/test.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome('D:\Darya\Python Test Automation\chromedriver_win32\chromedriver.exe')
+driver.get("https://wikipedia.org")
+search_field = driver.find_element_by_id("searchInput")
+search_field.send_keys("Test automation")
+search_button = driver.find_element_by_xpath("//*[@id='search-form']/fieldset/button/i")
+search_button.click()
+
+assert "Test automation" in driver.title
+
+driver.quit()
